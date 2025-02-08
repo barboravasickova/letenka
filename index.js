47651d9708197a3ad7802b32e00337a5aea341ef
Barbora Vašíčková <vasickovabara@gmail.com>
2025-02-08 12:05:09 +0100

    1 - Startovni a cilova zeme

--- index.js
+++ index.js
@@ -50,3 +50,16 @@ const flight = {
     },
     quality: 66.66659,
   };
+  
+/* Proveďte následující:
+
+1 - Vypište do stránky název startovní a cílové země.
+2 - Uložte do separátní proměnné objekt udávající maximální rozměry zavazadel.
+3 - Z objektu s rozměry zavazadel vytáhněte maximální povolené rozměry příručního zavazadla a vypište tyto rozměry opět jeden po druhém vypište do stránky.
+4 - Vypište do stránky, kolik cestující zaplatí za druhé zavazadlo v českých korunách zaokrouhleno nahoru na celé koruny.
+5 - Pokud máte chuť, malinko stránku nastylujte, aby se uživatel v informacích vyznal.
+
+*/
+
+document.body.innerHTML += '<p>Startovní země: ' + flight.countryFrom.name + '</p>';
+document.body.innerHTML += '<p>Cílová země: ' + flight.countryTo.name + '</p>';
